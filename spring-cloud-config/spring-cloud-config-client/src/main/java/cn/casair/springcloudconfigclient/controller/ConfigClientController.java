a260ef49852052eb2abc7c4158516634526216ab
Seas <seascheng@Mac.local>
2019-05-20 17:05:02 +0800

    Add spring cloud gateway

--- spring-cloud-config/spring-cloud-config-client/src/main/java/cn/casair/springcloudconfigclient/controller/ConfigClientController.java
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/cn/casair/springcloudconfigclient/controller/ConfigClientController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 // 启用了refresh配置文件的功能
 // 配置文件变动后，先执行post请求：
 //  curl -X POST http://localhost:8521/actuator/refresh  （8521是该client的port）
-// 客户端即会自动刷新RefreshScope的接口，获取到最新配置
+// 当前客户端在调用RefreshScope的接口时，即获取最新配置
 
 // 但如果使用spring cloud bus。就可以执行server端请求后：
 // curl -X POST http://localhost:8520/actuator/bus-refresh/  （8520是server的port）
-// 这样客户端即可自动刷新RefreshScope的接口，获取最新配置
+// 所有客户端在调用RefreshScope的接口时，即全部获取最新配置
 @RestController
 @RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class ConfigClientController {
---
+++ spring-cloud-gateway/spring-cloud-gateway-gw/src/main/java/cn/casair/springcloudgatewaygw/SpringCloudGatewayGwApplication.java
@@ -0,0 +1,13 @@
+package cn.casair.springcloudgatewaygw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayGwApplication.class, args);
+    }
+
+}
