7233e60fc6f3fb296c122cd72429d9521ac39c75
Seas <seascheng@Mac.local>
2019-05-20 15:21:41 +0800

    Add spring cloud bus

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spring-cloud-demo
+
+
+
+1. Spring Cloud 与Spring Boot、其他组件的对应关系：
+
+   https://github.com/spring-projects/spring-cloud/wiki
+
+2. Spring Boot  Actuator endpoints
+
+   https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-endpoints.html
+
+   Actuator endpoints let you monitor and interact with your application. Spring Boot includes a number of built-in endpoints and lets you add your own.
--- spring-cloud-config/spring-cloud-config-client/src/main/java/cn/casair/springcloudconfigclient/controller/ConfigClientController.java
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/cn/casair/springcloudconfigclient/controller/ConfigClientController.java
@@ -5,6 +5,11 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 启用了refresh配置文件的功能
+// 配置文件变动后，先执行post请求：
+//  curl -X POST http://localhost:8521/actuator/refresh
+// 再刷新接口，/env即可拿到最新的配置
+
 @RestController
 @RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class ConfigClientController {
